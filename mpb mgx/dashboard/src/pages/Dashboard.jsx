8e91b6eb8a1a01515b3030f11ccfd42e57f56f18
Tahj <tahjholden@gmail.com>
2025-06-15 01:05:56 -0400

    Delete mpb mgx directory
    Old files didn't work with Vercel

--- mpb mgx/dashboard/src/pages/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-// Dashboard.jsx
-import React from 'react';
-import NewDashboard from '../components/dashboard/NewDashboard';
-
-const Dashboard = () => {
-  return <NewDashboard />;
-};
-
-export default Dashboard;
